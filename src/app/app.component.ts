cc33831e09272cb0017b261ef567d779e7c5cc99
Thomas-Laurenz Schulte <thomas-laurenz.schulte@adesso.de>
2024-08-07 16:21:56 +0200

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, NgZone } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,7 +7,29 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   public title = 'EstimateUai';
+  protected counter = 0;
+
+  constructor(private ngZone: NgZone) {
+    ngZone.onMicrotaskEmpty.subscribe((): void => {
+      console.log('onMicrotaskEmpty');
+    });
+
+    this.ngZone.runOutsideAngular((): void => {
+      setInterval((): void => {
+        this.counter++;
+      }, 3000);
+    });
+  }
+
+  protected up(): void {
+    this.counter++;
+  }
+
+  protected down(): void {
+    this.counter--;
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  // provideZoneChangeDetection,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    //provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
   ],
 };
