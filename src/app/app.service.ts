6b16aba240135b62fc4063a4aecd23c6edeef554
Thomas-Laurenz Schulte <thomas-laurenz.schulte@adesso.de>
2024-08-08 15:05:46 +0200

    web-worker update

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppService {
+  private appWorker: Worker;
+
   constructor(private readonly http: HttpClient) {
-    if (typeof Worker !== 'undefined') {
-      const worker: Worker = new Worker(
-        new URL('./app.worker', import.meta.url),
-      );
+    this.appWorker = new Worker(new URL('./app.worker', import.meta.url));
 
-      worker.onmessage = ({ data }) => {
-        console.log(data);
-      };
+    this.appWorker.onmessage = ({ data }: Record<'data', number>): void => {
+      console.log(data);
+    };
 
-      http.get('http://localhost:3000/posts').subscribe((value: object) => {
-        worker.postMessage(value);
+    this.postsMessage();
+  }
+
+  private postsMessage(): void {
+    this.http
+      .get<Record<'range', number>>('http://localhost:3000/profile')
+      .pipe(map((result: Record<'range', number>) => result.range))
+      .subscribe((range: number): void => {
+        this.appWorker.postMessage(range);
       });
-    } else {
-      // Web Workers are not supported in this environment.
-      // You should add a fallback so that your program still executes correctly.
-    }
   }
 }
--- src/app/app.worker.ts
+++ src/app/app.worker.ts
@@ -1,6 +1,25 @@
 /// <reference lib="webworker" />
 
-addEventListener('message', ({ data }) => {
-  console.log(typeof data);
-  postMessage(data);
+import { setBigInt } from './big-int';
+
+addEventListener('message', ({ data }: Record<'data', number>): void => {
+  getData().catch(console.error);
+
+  postMessage(setBigInt(data));
 });
+
+const getData = async (): Promise<void> => {
+  const url = 'http://localhost:3000/posts';
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    console.log(await response.json());
+  } catch (error) {
+    console.error(error);
+  }
+};
