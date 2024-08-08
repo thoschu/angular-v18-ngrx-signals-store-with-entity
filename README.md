3abe4a0466c7e1433019f164972562cc74c801e6
Thomas-Laurenz Schulte <thomas-laurenz.schulte@adesso.de>
2024-08-08 21:46:14 +0200

    @angular/pwa added (service-worker)

--- README.md
+++ README.md
@@ -31,6 +31,8 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
 
 ## Info
 
-### Web-Worker
+### Service-Worker
 
-> ng g web-worker app
+> - https://www.npmjs.com/package/@angular/pwa
+> 
+> > ng add @angular/pwa --project <project-name>
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,34 @@
 import {
   ApplicationConfig,
   provideExperimentalZonelessChangeDetection,
+  isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
