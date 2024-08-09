bc29907264fce9bf181e96c6df205b78468980ae
Thomas-Laurenz Schulte <thomas-laurenz.schulte@adesso.de>
2024-08-09 14:05:59 +0200

    update app and refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,11 @@ import {
   WritableSignal,
 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AppService } from './app.service';
 import { JsonPipe } from '@angular/common';
+import { SwUpdate } from '@angular/service-worker';
+
+import { AppService } from './app.service';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -22,20 +25,68 @@ import { JsonPipe } from '@angular/common';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  public title = 'EstimateUai';
+  public title = ' EstimateUai ';
   protected counter: WritableSignal<number> = signal<number>(0);
-  protected doubleCounter: Signal<number> = computed(() => this.counter() * 2);
+  protected doubleCounter: Signal<number> = computed(
+    (): number => this.counter() * 2,
+  );
 
   constructor(
     private readonly injector: Injector,
+    private readonly swUpdate: SwUpdate,
     protected readonly appService: AppService,
   ) {
     this.counter.set(1);
+
+    interval(4000).subscribe((): void => {
+      this.swUpdate
+        .checkForUpdate()
+        .then((isUpdateAvailable: boolean): void => {
+          if (isUpdateAvailable) {
+            document.location.reload();
+          }
+        })
+        .catch(console.error)
+        .finally(console.info);
+    });
+
+    // this.swUpdate.versionUpdates
+    //   .pipe(
+    //     tap(console.log),
+    //     tap((versionEvent: VersionEvent): void => {
+    //       if ('version' in versionEvent && versionEvent.version) {
+    //         console.log(`App-Version: ${versionEvent.version.hash}`);
+    //       }
+    //     }),
+    //     filter(
+    //       (versionEvent: VersionEvent): boolean =>
+    //         versionEvent.type === 'VERSION_READY',
+    //     ),
+    //   )
+    //   .subscribe((versionEvent: VersionEvent): void => {
+    //     let text = `New version for the app is available. Do you want to reload?`;
+    //
+    //     if (
+    //       'currentVersion' in versionEvent &&
+    //       versionEvent.currentVersion &&
+    //       'latestVersion' in versionEvent &&
+    //       versionEvent.latestVersion
+    //     ) {
+    //       const latestVersion = `${versionEvent.latestVersion.hash}`,
+    //         currentVersion = `${versionEvent.currentVersion.hash}`;
+    //
+    //       text = `New version: ${latestVersion} for the app is available. Do you want to reload version: ${currentVersion} ?`;
+    //     }
+    //
+    //     if (confirm(text)) {
+    //       document.location.reload();
+    //     }
+    //   });
   }
 
   private unusedButtonClick(): void {
     const effectRef: EffectRef = effect(
-      () => {
+      (): void => {
         const val: number = this.counter() * 10;
         console.info(val);
         effectRef.destroy();
@@ -47,12 +98,12 @@ export class AppComponent {
   }
 
   protected up(): void {
-    this.counter.update((counter: number) => ++counter);
+    this.counter.update((counter: number): number => ++counter);
     this.unusedButtonClick();
   }
 
   protected down(): void {
-    this.counter.update(() => {
+    this.counter.update((): number => {
       return this.counter() - 1;
     });
 
