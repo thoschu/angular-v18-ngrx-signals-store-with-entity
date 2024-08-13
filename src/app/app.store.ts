7a5e6b553e1a2ef3deaec07e5d4d01fe627d91e5
Thomas-Laurenz Schulte <thomas-laurenz.schulte@adesso.de>
2024-08-13 12:22:53 +0200

    rxMethod from '@ngrx/signals/rxjs-interop' added

--- src/app/app.store.ts
+++ src/app/app.store.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
+import { tapResponse } from '@ngrx/operators';
 import {
-  // getState,
+  getState,
   patchState,
   signalStore,
   withComputed,
@@ -8,19 +9,38 @@ import {
   withMethods,
   withState,
 } from '@ngrx/signals';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { computed, effect, inject } from '@angular/core';
 import { dec, inc } from 'ramda';
+import { pipe, switchMap } from 'rxjs';
+
+type Comment = Record<'id' | 'text' | 'postId', string>;
+type Comments = Comment[];
+
+type Post = Record<'id' | 'title', string> & Record<'views', number>;
+type Posts = Post[];
+
+type Profile = Record<'name', string> & Record<'range', number>;
 
 interface AppState {
   title: string;
   counter: number;
   isLoading: boolean;
+  comments: Comments;
+  posts: Posts;
+  profile: Profile;
 }
 
 const initialAppState: AppState = {
   title: 'EstimateUai',
   counter: 0,
   isLoading: false,
+  comments: [],
+  posts: [],
+  profile: {
+    name: '1',
+    range: 0,
+  },
 };
 
 export const AppStore = signalStore(
@@ -30,39 +50,74 @@ export const AppStore = signalStore(
     uppercaseTitle: computed(() => title().toUpperCase()),
     doubleCounter: computed((): number => counter() * 2),
   })),
-  withMethods((store) => {
-    const httpClient: HttpClient = inject<HttpClient>(HttpClient);
+  withMethods(
+    (store, httpClient: HttpClient = inject<HttpClient>(HttpClient)) => {
+      return {
+        async loadAll() {
+          // const appResult = await appService.getItems();
+          // patchState(store, { app: '####' });
+        },
+        getComments: rxMethod<void>(
+          pipe(
+            switchMap(() =>
+              httpClient.get<Comments>('http://localhost:3000/comments').pipe(
+                tapResponse(
+                  (comments: Comments) => {
+                    patchState(store, { comments });
+                  },
+                  (err: Error) => console.error(err),
+                ),
+              ),
+            ),
+          ),
+        ),
+        _loadComments() {
+          return httpClient
+            .get<Comments>('http://localhost:3000/comments')
+            .pipe()
+            .subscribe((comments: Comments) => {
+              patchState(store, { comments });
+            });
+        },
+        _loadPosts() {
+          return httpClient
+            .get<Posts>('http://localhost:3000/posts')
+            .pipe()
+            .subscribe((posts: Posts) => {
+              patchState(store, { posts });
+            });
+        },
+        _loadProfile() {
+          return httpClient
+            .get<Profile>('http://localhost:3000/profile')
+            .pipe()
+            .subscribe((profile: Profile) => {
+              patchState(store, { profile });
+            });
+        },
+        setCounter(counter: number): void {
+          patchState(store, { counter });
+        },
+        decrementCounter(): void {
+          const counter: number = dec(store.counter());
 
-    return {
-      async loadAll() {
-        // const appResult = await appService.getItems();
-        // patchState(store, { app: '####' });
-        httpClient.get('http://localhost:3000/comments').subscribe((res) => {
-          console.log(res);
-        });
-      },
-      setCounter(counter: number): void {
-        patchState(store, { counter });
-      },
-      decrementCounter(): void {
-        const counter: number = dec(store.counter());
-
-        patchState(store, { counter });
-      },
-      incrementCounter(): void {
-        const counter: number = inc(store.counter());
-
-        patchState(store, { counter });
-      },
-    };
-  }),
+          patchState(store, { counter });
+        },
+        incrementCounter(): void {
+          patchState(store, { counter: inc(store.counter()) });
+        },
+      };
+    },
+  ),
   withHooks({
-    onInit({ loadAll }: { loadAll: () => Promise<unknown> }) {
-      loadAll().then((r) => console.log(r));
+    onInit(store) {
+      store._loadComments();
+      store._loadPosts();
+      store._loadProfile();
 
       effect(() => {
-        // console.log('counter state', state);
-      });
+        console.log('[effect] counter state', getState(store));
+      }); // logs: { count: 2 }
     },
     onDestroy() {
       console.log('on destroy');
